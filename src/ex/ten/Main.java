20a9a4164bf57a558a949d4df22a5b67fb89a3d0
Tyutyakova <p-v-a19@mail.ru>
2024-03-06 18:35:35 +0300

    find char

--- src/ex/ten/Main.java
+++ src/ex/ten/Main.java
@@ -7,6 +7,10 @@ public class Main {
         Worker workOne = new Worker( 40.0,"PIik",20.0);
         Manager one = new Manager( "Nik", 300.0, 0);
         Director main = new Director("Leo", 300.0, 0);
+        Employee emOne1 = new Manager(22.4,"Lisa",33.3,5 );
+        Employee[] arr = {emOne1,workOne,one,main};
+        System.out.println(main.getSalary());
+
 
         System.out.println(emOne.getSalary());
         System.out.println(workOne.getSalary());
@@ -14,6 +18,21 @@ public class Main {
         System.out.println(one.getName());
         System.out.println(main.getSalary());
 
+
+        System.out.println(getCount("aeuut ttor"));
     }
 
-}
+    public static int getCount(String str) {
+        int count = 0;
+       for (int i = 0; str.length() > i; i++) {
+           char ch = str.charAt(i);
+           if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+               count++;
+           }
+       } return count;
+
+    }
+
+    }
+
+
