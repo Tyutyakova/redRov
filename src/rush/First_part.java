05c81208964b6e8f0e01badc2c52b960bbe3035e
Tyutyakova <p-v-a19@mail.ru>
2024-10-14 16:06:12 +0300

    rush

--- src/rush/First_part.java
+++ src/rush/First_part.java
@@ -183,6 +183,21 @@ public class First_part {
 
         System.out.println(secondMin);
     }*/
+ /*       Scanner scanner = new Scanner(System.in);
+        int start = scanner.nextInt();
+        int end = scanner.nextInt();
+        int multiple = scanner.nextInt();
+
+        int sum = 0;
+        //напишите тут ваш код
+        for(int i = start; i < end; i++) {
+            if (i % multiple != 0 ) continue;
+                sum += i;
+
+        }
+
+        System.out.println(sum);*/
+
 
 
         }
